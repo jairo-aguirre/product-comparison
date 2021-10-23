2d0286aa8fc2482a9db4ca8980ccee50e3e5f9c4
Lisa Cormier <lcormier5@hotmail.com>
2021-10-23 17:46:36 -0600

    add local valuables and require variables into seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'rest-client'
-require 'json'
-url = 'https://asos2.p.rapidapi.com/products/v2/list?store=US&offset=0&categoryId=4209&limit=48&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US'
-uri = URI.parse(URI.encode(url.strip))
-
-rm = RestClient.get(
-  uri,  
-headers={
-  'x-rapidapi-host': 'asos2.p.rapidapi.com',
-  'x-rapidapi-key': '85cb3c3da5msh1736a5f390ea368p159ccejsn0b9cde09d29b'
-})
-
-rm_array = JSON.parse(rm)
-
-products_array = rm_array['products']
-
-File.open("product-list.rb", "w"){ |f| f.write products_array }
-
-#products_array.each do |product|
- # Product.create(
-  #  name: product['name'],
-   # image: product['imageUrl'],
-   # price_cents: product['price']['current']['value']
-  #)
-#end
+require 'category-list'
+require 'product-list'
