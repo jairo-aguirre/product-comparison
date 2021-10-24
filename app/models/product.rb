3cebe7282fc4ecf1abd9505880effd8751983d14
Lisa Cormier <lcormier5@hotmail.com>
2021-10-24 14:44:58 -0600

    functional database

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  belongs_to :categories, optional: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,110 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require './db/categories-decoded'
+require './db/products-decoded'
+
+@categories_unique.each do |category|
+  Category.create!(
+    
+    name: category[:name]
+    )
+end
+
+cat1 = Category.find_or_create_by! id: 1
+cat2 = Category.find_or_create_by! id: 2
+cat3 = Category.find_or_create_by! id: 3
+cat4 = Category.find_or_create_by! id: 4
+cat5 = Category.find_or_create_by! id: 5
+cat6 = Category.find_or_create_by! id: 6
+cat7 = Category.find_or_create_by! id: 7
+
+
+@products_modified.each do |product|
+  if product[:category_id] = 1
+  cat1.products.create!(
+    name: product[:name],
+    description: product[:description],
+    image: product[:image],
+    price_cents: product[:price_cents],
+    rating: product[:rating],
+    sale: product[:sale],
+    url: product[:url]
+    
+  )
+  end
+  if product[:category_id] = 2
+    cat2.products.create!(
+      name: product[:name],
+      description: product[:description],
+      image: product[:image],
+      price_cents: product[:price_cents],
+      rating: product[:rating],
+      sale: product[:sale],
+      url: product[:url]
+      
+    )
+    end
+    if product[:category_id] = 3
+      cat3.products.create!(
+        name: product[:name],
+        description: product[:description],
+        image: product[:image],
+        price_cents: product[:price_cents],
+        rating: product[:rating],
+        sale: product[:sale],
+        url: product[:url]
+        
+      )
+      end
+      if product[:category_id] = 4
+        cat4.products.create!(
+          name: product[:name],
+          description: product[:description],
+          image: product[:image],
+          price_cents: product[:price_cents],
+          rating: product[:rating],
+          sale: product[:sale],
+          url: product[:url]
+          
+        )
+        end
+        if product[:category_id] = 5
+          cat5.products.create!(
+            name: product[:name],
+            description: product[:description],
+            image: product[:image],
+            price_cents: product[:price_cents],
+            rating: product[:rating],
+            sale: product[:sale],
+            url: product[:url]
+            
+          )
+          end
+          if product[:category_id] = 6
+            cat6.products.create!(
+              name: product[:name],
+              description: product[:description],
+              image: product[:image],
+              price_cents: product[:price_cents],
+              rating: product[:rating],
+              sale: product[:sale],
+              url: product[:url]
+              
+            )
+            end
+            if product[:category_id] = 7
+              cat7.products.create!(
+                name: product[:name],
+                description: product[:description],
+                image: product[:image],
+                price_cents: product[:price_cents],
+                rating: product[:rating],
+                sale: product[:sale],
+                url: product[:url]
+                
+              )
+              end
+  
+end
