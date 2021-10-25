d6a7028953e8baf46b89267d5e4520bfca6eca8a
Teenaelza <teena.thaliath@gmail.com>
2021-10-25 10:11:47 -0700

    Feature-showing

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,8 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    render :json => {
+      categories: @categories
+  }
+  end
+end
--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,43 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
-
-const options = {
-  method: 'GET',
-  url: 'https://asos2.p.rapidapi.com/products/v2/list',
-  params: {
-    store: 'US',
-    offset: '0',
-    categoryId: '4209',
-    limit: '48',
-    country: 'US',
-    sort: 'freshness',
-    currency: 'USD',
-    sizeSchema: 'US',
-    lang: 'en-US'
-  },
-  headers: {
-    'x-rapidapi-host': 'asos2.p.rapidapi.com',
-    'x-rapidapi-key': '85cb3c3da5msh1736a5f390ea368p159ccejsn0b9cde09d29b'
-  }
-};
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
-
-  
-  
-  
-  fetchData = () => {
-    axios.request(options) // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log('reponsedata:', response.data) // The entire response from the Rails API
-
-      console.log(response.data) // Just the message
-      this.setState({
-        message: JSON.stringify(response.data)
-      });
-    }) 
-  }
-
-  render() {
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Product from "./components/Product";
+import Category from "./components/Category";
+export default function Application(props) {
+  const [state, setState] = useState({ products: [], categories: [] });
+  useEffect(() => {
+    const URL1 = "http://localhost:3000/";
+    const URL2 = "http://localhost:3000/categories/";
+    Promise.all([axios.get(URL1), axios.get(URL2)]).then((all) => {
+      console.log(all);
+      const [first, second] = all;
+      const products = first.data.products;
+      const categories = second.data.categories;
+      console.log(categories);
+      setState((prev) => ({ ...prev, products, categories }));
+    });
+  }, []);
+  const firstset = state.products.slice(0, 10);
+  const productArray = firstset.map((product) => {
     return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
+      <Product
+        key={product.id}
+        id={product.id}
+        name={product.name}
+        image={product.image}
+      />
     );
-  }
+  });
+  const categoryArray = state.categories.map((category) => {
+    return <Category key={category.id} id={category.id} name={category.name} />;
+  });
+  return (
+    <div className="container">
+      <h1>Products</h1>
+      <hr />
+      <br></br>
+      <div className="row">{categoryArray}</div>
+      <div className="row">{productArray}</div>
+    </div>
+  );
 }
-
-export default App;
